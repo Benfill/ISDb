b23d04ae30d4f6daef270fc85200bf37aa3acee3
Anass Benfillous <anasse.benfillous@gmail.com>
2024-12-12 14:29:43 +0100

    add song search by title functionality

--- src/main/java/io/benfill/isdb/repository/SongRepository.java
+++ src/main/java/io/benfill/isdb/repository/SongRepository.java
@@ -1,5 +1,8 @@
 package io.benfill.isdb.repository;
 
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +10,6 @@ import io.benfill.isdb.model.Song;
 
 @Repository
 public interface SongRepository extends MongoRepository<Song, Long> {
+	List<Song> findByTitleLike(String title, Pageable pageable);
 
 }
