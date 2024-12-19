df162d674d46f7f26216d793111649df71127638
Anass Benfillous <anasse.benfillous@gmail.com>
2024-12-19 13:02:59 +0100

    handle login

---
+++ src/main/java/io/benfill/isdb/model/AuthToken.java
@@ -0,0 +1,19 @@
+package io.benfill.isdb.model;
+
+import java.util.Set;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Represents an authentication token.
+ */
+@Data
+@Builder
+public class AuthToken {
+	private String name;
+	private String username;
+	private Set<Role> roles;
+	private String token;
+
+}
